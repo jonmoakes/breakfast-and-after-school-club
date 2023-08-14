d558e1fa3450b8db5c14e311f7a011e48b3824f8
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2023-08-14 09:55:32 +0100

    test to stop input over 100

--- src/routes/add-funds/add-funds-hooks/use-handle-wallet-funds-change.js
+++ src/routes/add-funds/add-funds-hooks/use-handle-wallet-funds-change.js
@@ -1,12 +1,39 @@
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 
+// import { setWalletFundsToAdd } from "../../../store/wallet-funds-to-add/wallet-funds-to-add.slice";
+
+// const useHandleWalletFundsChange = () => {
+//   const dispatch = useDispatch();
+
+//   const handleWalletFundsChange = (event) => {
+//     dispatch(setWalletFundsToAdd(Number(event.target.value)));
+//   };
+
+//   return { handleWalletFundsChange };
+// };
+
+// export default useHandleWalletFundsChange;
+import { useDispatch } from "react-redux";
 import { setWalletFundsToAdd } from "../../../store/wallet-funds-to-add/wallet-funds-to-add.slice";
 
 const useHandleWalletFundsChange = () => {
   const dispatch = useDispatch();
 
   const handleWalletFundsChange = (event) => {
-    dispatch(setWalletFundsToAdd(Number(event.target.value)));
+    const enteredValue = event.target.value;
+
+    // Parse the entered value as a number
+    const value = parseFloat(enteredValue);
+
+    // Check for valid numeric value
+    if (isNaN(value)) {
+      return;
+    }
+
+    // Enforce a maximum value of 100
+    const clampedValue = Math.min(value, 100);
+
+    dispatch(setWalletFundsToAdd(clampedValue));
   };
 
   return { handleWalletFundsChange };
