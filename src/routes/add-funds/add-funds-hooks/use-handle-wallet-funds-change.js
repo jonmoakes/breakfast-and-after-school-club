d577203ca77790ed305873b5b0cca54c2bf8287a
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2023-08-14 09:40:44 +0100

    edited handlechange for decimal

--- src/routes/add-funds/add-funds-hooks/use-handle-wallet-funds-change.js
+++ src/routes/add-funds/add-funds-hooks/use-handle-wallet-funds-change.js
@@ -1,29 +1,3 @@
-// import { useDispatch } from "react-redux";
-// import { setWalletFundsToAdd } from "../../../store/wallet-funds-to-add/wallet-funds-to-add.slice";
-
-// const useHandleWalletFundsChange = () => {
-//   const dispatch = useDispatch();
-
-//   const handleWalletFundsChange = (event) => {
-//     const enteredValue = event.target.value;
-
-//     const pattern = /^\d*$/;
-//     if (!pattern.test(enteredValue)) {
-//       return; // Invalid pattern (non-digit characters)
-//     }
-
-//     const value = Number(enteredValue, 10);
-//     if (value > 100) {
-//       return;
-//     }
-
-//     dispatch(setWalletFundsToAdd(value));
-//   };
-
-//   return { handleWalletFundsChange };
-// };
-
-// export default useHandleWalletFundsChange;
 import { useDispatch } from "react-redux";
 import { setWalletFundsToAdd } from "../../../store/wallet-funds-to-add/wallet-funds-to-add.slice";
 
@@ -32,6 +6,11 @@ const useHandleWalletFundsChange = () => {
 
   const handleWalletFundsChange = (event) => {
     const enteredValue = event.target.value;
+
+    const pattern = /^(\d*\.?\d*)$/;
+    if (!pattern.test(enteredValue)) {
+      return; // Invalid pattern (non-digit characters or incorrect decimal format)
+    }
 
     const value = parseFloat(enteredValue); // Parse decimal values
 
