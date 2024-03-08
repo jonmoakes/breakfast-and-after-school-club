260c396c8465fe4aecb12cab594e62bf272df16c
Jonathan <jonathanmartynoakes@gmail.com>
2024-03-08 04:25:37 +0000

    moved set project id back to config file

--- src/utils/appwrite/appwrite-config.js
+++ src/utils/appwrite/appwrite-config.js
@@ -1,10 +1,19 @@
 import { Client, Account, Databases, Query } from "appwrite";
-import { setProjectId } from "../../school-codes-list/get-ids-from-school-code/set-project-id";
+import { schoolCodesList } from "../../school-codes-list/school-codes-list";
 
 export const client = new Client();
 client.setEndpoint("https://appwrite.breakfast-and-after-school-club.co.uk/v1");
 
-setProjectId();
+const { cleveleysPrimary } = schoolCodesList;
+const schoolCode = localStorage.getItem("schoolCode");
+
+switch (schoolCode) {
+  case cleveleysPrimary:
+    client.setProject(import.meta.env.VITE_CLEVELEYS_PRIMARY_PROJECT_ID);
+    break;
+  default:
+    client.setProject(import.meta.env.VITE_TEST_SCHOOL_PROJECT_ID);
+}
 
 export const account = new Account(client);
 export const databases = new Databases(client);
