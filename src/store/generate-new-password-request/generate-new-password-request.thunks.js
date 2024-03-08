f5dac8656e02d427d000d20f28b249219b9a1ce4
Jonathan <jonathanmartynoakes@gmail.com>
2024-03-08 16:34:08 +0000

    more testing

--- src/store/generate-new-password-request/generate-new-password-request.thunks.js
+++ src/store/generate-new-password-request/generate-new-password-request.thunks.js
@@ -11,15 +11,23 @@ export const generateNewPasswordRequestAsync = createAsyncThunk(
   async ({ generateNewPasswordRequestEmail }, thunkAPI) => {
     try {
       if (import.meta.env.MODE === "development") {
-        await account.createRecovery(
-          generateNewPasswordRequestEmail,
-          localhostChooseNewPasswordRoute
-        );
+        const devTest = async () => {
+          await account.createRecovery(
+            generateNewPasswordRequestEmail,
+            localhostChooseNewPasswordRoute
+          );
+        };
+        const result = await devTest();
+        console.log("dev result ", result);
       } else if (import.meta.env.MODE === "production") {
-        await account.createRecovery(
-          generateNewPasswordRequestEmail,
-          `https://www.breakfast-and-after-school-club.co.uk${chooseNewPasswordRoute}`
-        );
+        const prodTest = async () => {
+          await account.createRecovery(
+            generateNewPasswordRequestEmail,
+            `https://www.breakfast-and-after-school-club.co.uk${chooseNewPasswordRoute}`
+          );
+        };
+        const prodResult = await prodTest();
+        console.log("prod result ", prodResult);
       }
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
