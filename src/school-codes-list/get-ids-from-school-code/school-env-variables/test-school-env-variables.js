01f8ff8298ed1ba5a4ed6d5fa917c7dd076be566
Jonathan <jonathanmartynoakes@gmail.com>
2024-04-15 21:17:28 +0100

    seperated school env variables into their own files

---
+++ src/school-codes-list/get-ids-from-school-code/school-env-variables/test-school-env-variables.js
@@ -0,0 +1,29 @@
+export const testSchoolEnvVariables = {
+  projectId: import.meta.env.VITE_TEST_SCHOOL_PROJECT_ID,
+  databaseId: import.meta.env.VITE_TEST_SCHOOL_DATABASE_ID,
+  stripePublishableKey: import.meta.env.VITE_STRIPE_TEST_SCHOOL_PUBLISHABLE_KEY,
+  stripeSecretKey: import.meta.env.VITE_STRIPE_TEST_SCHOOL_SECRET_KEY,
+  appOwnerEmail: import.meta.env.VITE_TEST_SCHOOL_SCHOOL_EMAIL,
+  appOwnerId: import.meta.env.VITE_TEST_SCHOOL_APP_OWNER_ID,
+  userCollectionId: import.meta.env.VITE_TEST_SCHOOL_USER_COLLECTION_ID,
+  termDatesCollectionId: import.meta.env
+    .VITE_TEST_SCHOOL_TERM_DATES_COLLECTION_ID,
+  earlyFinishDatesCollectionId: import.meta.env
+    .VITE_TEST_SCHOOL_EARLY_FINISH_DATES_COLLECTION_ID,
+  bookingClosingTimesCollectionId: import.meta.env
+    .VITE_TEST_SCHOOL_BOOKING_CLOSING_TIMES_COLLECTION_ID,
+  bookingClosingTimesDocumentId: import.meta.env
+    .VITE_TEST_SCHOOL_BOOKING_CLOSING_TIMES_DOCUMENT_ID,
+  sessionPricesCollectionId: import.meta.env
+    .VITE_TEST_SCHOOL_SESSION_PRICES_COLLECTION_ID,
+  sessionPricesDocumentId: import.meta.env
+    .VITE_TEST_SCHOOL_SESSION_PRICES_DOCUMENT_ID,
+  sessionTimesCollectionId: import.meta.env
+    .VITE_TEST_SCHOOL_SESSION_TIMES_COLLECTION_ID,
+  sessionTimesDocumentId: import.meta.env
+    .VITE_TEST_SCHOOL_SESSION_TIMES_DOCUMENT_ID,
+  childrenCollectionId: import.meta.env.VITE_TEST_SCHOOL_CHILDREN_COLLECTION_ID,
+  bookedSessionsCollectionId: import.meta.env
+    .VITE_TEST_SCHOOL_BOOKED_SESSIONS_COLLECTION_ID,
+  schoolLogoUrl: import.meta.env.VITE_TEST_SCHOOL_LOGO_URL,
+};
