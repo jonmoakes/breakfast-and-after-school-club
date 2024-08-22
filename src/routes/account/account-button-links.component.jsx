c804eca1421494feb2f853e4b986a25e2cc3c037
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2024-08-22 14:23:48 +0100

    updated manifest for better install compatibility

--- src/routes/account/account-button-links.component.jsx
+++ src/routes/account/account-button-links.component.jsx
@@ -5,10 +5,52 @@ import RenderButtonsList from "../../components/render-buttons-list/render-butto
 
 import { ParentDiv } from "../../styles/div/div.styles";
 import { BlueH2 } from "../../styles/h2/h2.styles";
+import { useEffect, useState } from "react";
 
 const AccountButtonLinks = () => {
   const { id, appOwnerId } = useGetCurrentUserSelectors();
   const { appOwnerButtons, notAppOwnerButtons } = useNavigateToRoute();
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [isInstallable, setIsInstallable] = useState(false);
+
+  console.log(deferredPrompt, isInstallable);
+  const handleInstallClick = async () => {
+    if (deferredPrompt) {
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      const choiceResult = await deferredPrompt.userChoice;
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+      // Clear the deferredPrompt since it can only be used once
+      setDeferredPrompt(null);
+      setIsInstallable(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Save the event so it can be triggered later
+      setDeferredPrompt(e);
+      // Update UI to notify the user they can install the PWA
+      setIsInstallable(true);
+    };
+
+    // Listen for the beforeinstallprompt event
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
 
   return (
     <>
@@ -21,6 +63,11 @@ const AccountButtonLinks = () => {
           <BlueH2>what would you like to do?</BlueH2>
           <RenderButtonsList {...{ buttons: notAppOwnerButtons }} />
         </ParentDiv>
+      )}
+      {!isInstallable ? null : (
+        <button onClick={handleInstallClick} className="install-button">
+          Install App
+        </button>
       )}
     </>
   );
